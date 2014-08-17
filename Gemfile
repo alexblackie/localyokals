081db380366d453835d253a69aa4c050069aafb2
Alex Blackie <alex@alexblackie.com>
2014-08-16 21:55:06 -0700

    Add environment file, sync gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem "middleman", "~>3.3.5"
+gem 'middleman', '~>3.3.5'
 
-gem "slim"
-gem "susy"
+gem 'middleman-dotenv'
+gem 'middleman-sync'
+
+gem 'slim'
+gem 'susy'
--- config.rb
+++ config.rb
@@ -3,14 +3,25 @@ require 'susy'
 activate :automatic_image_sizes
 activate :directory_indexes
 
-page "/", :layout => "home"
+page '/', :layout => 'home'
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
+set :css_dir,    'stylesheets'
+set :js_dir,     'javascripts'
 set :images_dir, 'images'
+
+# Activate sync extension
+activate :sync do |sync|
+  sync.fog_provider          = 'AWS'
+  sync.fog_directory         = ENV['AWS_BUCKET']
+  sync.fog_region            = ENV['AWS_REGION']
+  sync.aws_access_key_id     = ENV['AWS_ACCESS']
+  sync.aws_secret_access_key = ENV['AWS_SECRET']
+  sync.existing_remote_files = 'delete'
+  sync.after_build           = false
+end
 
 # Build-specific configuration
 configure :build do
-	activate :minify_css
+  activate :minify_css
   activate :minify_javascript
 end
