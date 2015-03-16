efa97860450711ec4c59cc1db654b513b196cad8
Alex Blackie <alex@alexblackie.com>
2015-03-16 10:29:26 -0700

    Switch to HAML, a lot of cleanup, add GA tracking

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'middleman', '~>3.3.7'
+gem "middleman", "~>3.3.7"
 
-gem 'middleman-dotenv'
-gem 'middleman-sync'
-gem 'susy'
+gem "middleman-dotenv"
+gem "middleman-sync"
+gem "susy"
--- config.rb
+++ config.rb
@@ -1,28 +1,28 @@
-require 'susy'
+require "susy"
 
 activate :dotenv
 activate :automatic_image_sizes
 activate :directory_indexes
 
-page '/', :layout => 'home'
+set :css_dir,    "stylesheets"
+set :js_dir,     "javascripts"
+set :images_dir, "images"
 
-set :css_dir,    'stylesheets'
-set :js_dir,     'javascripts'
-set :images_dir, 'images'
+set :haml, { ugly: true }
 
 # Activate sync extension
 activate :sync do |sync|
-  sync.fog_provider          = 'AWS'
-  sync.fog_directory         = ENV['AWS_BUCKET']
-  sync.fog_region            = ENV['AWS_REGION']
-  sync.aws_access_key_id     = ENV['AWS_ACCESS']
-  sync.aws_secret_access_key = ENV['AWS_SECRET']
-  sync.existing_remote_files = 'delete'
-  sync.after_build           = false
+	sync.fog_provider          = "AWS"
+	sync.fog_directory         = ENV["AWS_BUCKET"]
+	sync.fog_region            = ENV["AWS_REGION"]
+	sync.aws_access_key_id     = ENV["AWS_ACCESS"]
+	sync.aws_secret_access_key = ENV["AWS_SECRET"]
+	sync.existing_remote_files = "delete"
+	sync.after_build           = false
 end
 
 # Build-specific configuration
 configure :build do
-  activate :minify_css
-  activate :minify_javascript
+	activate :minify_css
+	activate :minify_javascript
 end
