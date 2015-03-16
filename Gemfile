0166fd321339a1aa7838f1a1282f3b0f8b4669d7
Alex Blackie <alex@alexblackie.com>
2015-03-16 12:20:43 -0700

    Deploy via rsync instead of S3/CF
    Cloudfront was too much to handle, and invalidations were *insanely*
    slow (which meant deploys took close to 20 minutes each).

--- Gemfile
+++ Gemfile
@@ -2,7 +2,5 @@ source "https://rubygems.org"
 
 gem "middleman", "~>3.3.10"
 
-gem "middleman-cloudfront"
-gem "middleman-dotenv"
-gem "middleman-sync"
+gem "middleman-deploy"
 gem "susy"
--- config.rb
+++ config.rb
@@ -1,6 +1,5 @@
 require "susy"
 
-activate :dotenv
 activate :automatic_image_sizes
 activate :directory_indexes
 
@@ -10,22 +9,12 @@ set :images_dir, "images"
 
 set :haml, { ugly: true }
 
-# Activate sync extension
-activate :sync do |sync|
-	sync.fog_provider          = "AWS"
-	sync.fog_directory         = ENV["AWS_BUCKET"]
-	sync.fog_region            = ENV["AWS_REGION"]
-	sync.aws_access_key_id     = ENV["AWS_ACCESS"]
-	sync.aws_secret_access_key = ENV["AWS_SECRET"]
-	sync.existing_remote_files = "delete"
-	sync.after_build           = false
-end
-
-activate :cloudfront do |cf|
-  cf.access_key_id     = ENV["AWS_ACCESS"]
-  cf.secret_access_key = ENV["AWS_SECRET"]
-  cf.distribution_id   = ENV["AWS_CFDIST"]
-  cf.after_build       = false
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host   = 'edge01'
+  deploy.path   = '/srv/www/localyokals.ca'
+  deploy.user   = 'deploy'
+  deploy.clean  = true
 end
 
 # Build-specific configuration
