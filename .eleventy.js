3e7f8a52050a2c0b547e390f8a4185188c3f3d36
Alex Blackie <alex@alexblackie.com>
2019-02-02 00:27:25 -0500

    Add shortcode for limiting code to prod builds
    Sometimes parts of the site only make sense to load in production -- for
    example, Google Analytics. For this, we should use a "paired shortcode"
    to wrap the whole content block in a conditional to only render it if a
    special "production" environment flag has been set.

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,12 @@ module.exports = function(config) {
   config.addPassthroughCopy("source/css");
   config.addPassthroughCopy("source/js");
 
+  config.addPairedShortcode("prod", function(content) {
+    if (process.env.ELEVENTY_PRODUCTION) {
+      return content;
+    }
+  });
+
   return {
     dir: {
       input: "source",
